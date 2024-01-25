911ab86a0098fa6fa4a9e3b47dedb5d45227b635
FahdAzouz <fahdstudies00@gmail.com>
2024-01-25 17:05:49 +0100

    task 5

--- web_flask/5-number_template.py
+++ web_flask/5-number_template.py
@@ -22,16 +22,19 @@ def hello_hbnb():
     """Displays Hello HBNB!"""
     return 'Hello HBNB!'
 
+
 @app.route('/hbnb', strict_slashes=False)
 def hbnb():
     """Displays HBNB!"""
     return 'HBNB!'
+
 
 @app.route('/c/<text>', strict_slashes=False)
 def c(text):
     """Displays C followed by the value of the text variable"""
     text = text.replace('_', ' ');
     return 'C {}'.format(text)
+
 
 @app.route('/python/', strict_slashes=False)
 @app.route('/python/<text>', strict_slashes=False)
@@ -40,15 +43,17 @@ def python(text="is cool"):
     text = text.replace('_', ' ');
     return 'Python {}'.format(text)
 
+
 @app.route('/number/<int:n>', strict_slashes=False)
 def number(n):
     """display “n is a number” only if n is an integer"""
     return "{} is a number".format(n)
 
+
 @app.route('/number_template/<int:n>', stric_slashes=False)
 def number_template(n):
     """display a HTML page only if n is an integer"""
-    return render_tempalate('templates/5-number.html', number=n)
+    return render_tempalate("5-number.html", n=n)
 
 
 if __name__ == '__main__':
