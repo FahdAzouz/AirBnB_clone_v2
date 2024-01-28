990b3d84bf16c4d461a75a6041fb395e4b9ed1b4
FahdAzouz <fahdstudies00@gmail.com>
2024-01-28 17:32:21 +0100

    task 1

--- web_flask/1-hbnb_route.py
+++ web_flask/1-hbnb_route.py
@@ -1,10 +1,10 @@
 #!/usr/bin/python3
-"""Starts a flask applicatioon.
+"""Starts a Flask web application.
 
-The application listens on 0.0.0.0, port 5000
+The application listens on 0.0.0.0, port 5000.
 Routes:
-    /: Displays 'Hello HBNB!'
-    /hbnb: Displays "HBNB"
+    /: Displays 'Hello HBNB!'.
+    /hbnb: Displays 'HBNB'.
 """
 from flask import Flask
 
@@ -13,15 +13,15 @@ app = Flask(__name__)
 
 @app.route("/", strict_slashes=False)
 def hello_hbnb():
-    """Displays Hello HBNB!"""
+    """Displays 'Hello HBNB!'."""
     return "Hello HBNB!"
 
 
 @app.route("/hbnb", strict_slashes=False)
 def hbnb():
-    """Displays HBNB!"""
-    return "HBNB!"
+    """Displays 'HBNB'."""
+    return "HBNB"
 
 
 if __name__ == "__main__":
-    app.run(host='0.0.0.0')
+    app.run(host="0.0.0.0")
