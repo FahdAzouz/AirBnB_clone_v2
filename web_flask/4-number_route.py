4025028a2fa548175650e7fb1aaf0927dcc507b8
FahdAzouz <fahdstudies00@gmail.com>
2024-01-28 17:36:28 +0100

    task 4

--- web_flask/4-number_route.py
+++ web_flask/4-number_route.py
@@ -20,23 +20,27 @@ def hello_hbnb():
     """Displays Hello HBNB!"""
     return 'Hello HBNB!'
 
+
 @app.route('/hbnb', strict_slashes=False)
 def hbnb():
     """Displays HBNB!"""
     return 'HBNB!'
 
+
 @app.route('/c/<text>', strict_slashes=False)
 def c(text):
     """Displays C followed by the value of the text variable"""
-    text = text.replace('_', ' ');
+    text = text.replace('_', ' ')
     return 'C {}'.format(text)
+
 
 @app.route('/python/', strict_slashes=False)
 @app.route('/python/<text>', strict_slashes=False)
 def python(text="is cool"):
     """Displays Python followed by the value of the text variable"""
-    text = text.replace('_', ' ');
+    text = text.replace('_', ' ')
     return 'Python {}'.format(text)
+
 
 @app.route('/number/<int:n>', strict_slashes=False)
 def number(n):
