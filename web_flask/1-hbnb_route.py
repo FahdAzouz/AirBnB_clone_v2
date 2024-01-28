bb1de2bb41ad16a5485a9d71d57545facfc76798
FahdAzouz <fahdstudies00@gmail.com>
2024-01-28 17:27:58 +0100

    task 1

--- web_flask/1-hbnb_route.py
+++ web_flask/1-hbnb_route.py
@@ -16,6 +16,7 @@ def hello_hbnb():
     """Displays Hello HBNB!"""
     return 'Hello HBNB!'
 
+
 @app.route('/hbnb', strict_slashes=False)
 def hbnb():
     """Displays HBNB!"""
@@ -23,4 +24,4 @@ def hbnb():
 
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)
+    app.run(host='0.0.0.0')
